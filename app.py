fa705b52675d72852fdb04bda6beeeebaa3372ec
Kristjan Tamm <90192493+TammKristjan@users.noreply.github.com>
2023-05-15 02:38:28 +0300

    Forms

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Length, NumberRange
 import sqlite3
 from flask import g
+from forms import CompanyForm, IncreaseCapitalForm
 
 
 app = Flask(__name__)
@@ -55,23 +53,6 @@ def create_tables():
         )
     ''')
     db.commit()
-
-
-class CompanyForm(FlaskForm):
-    name = StringField('Nimi', validators=[DataRequired(), Length(min=3, max=100)])
-    reg_code = StringField('Registrikood', validators=[DataRequired(), Length(min=7, max=7)])
-    founding_date = DateField('Asutamiskuupäev', validators=[DataRequired()], format='%Y-%m-%d')
-    capital = IntegerField('Kogukapital', validators=[DataRequired(), NumberRange(min=2500)])
-    shareholder_name = StringField('Osaniku nimi', validators=[DataRequired(), Length(min=3, max=100)])
-    shareholder_personalcode = IntegerField('Osaniku isikukood/registrikood', validators=[DataRequired(), NumberRange(min=1000000, max=99999999999)])
-    shareholder_share = IntegerField('Osaniku osa', validators=[DataRequired(), NumberRange(min=1)])
-
-
-class IncreaseCapitalForm(FlaskForm):
-    shareholder_name = StringField('Osaniku nimi', validators=[DataRequired(), Length(min=3, max=100)])
-    shareholder_personalcode = IntegerField('Osaniku isikukood/registrikood', validators=[DataRequired(), NumberRange(min=1000000, max=99999999999)])
-    shareholder_share = IntegerField('Osaniku osa', validators=[DataRequired(), NumberRange(min=1)])
-
 
 # Andmebaasina kasutame siin lihtsat sõnastikku
 companies = []
--- forms.py
+++ forms.py
@@ -1,21 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, BooleanField, FieldList, FormField
+from wtforms import StringField, DateField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, NumberRange
 
+class CompanyForm(FlaskForm):
+    name = StringField('Nimi', validators=[DataRequired(), Length(min=3, max=100)])
+    reg_code = StringField('Registrikood', validators=[DataRequired(), Length(min=7, max=7)])
+    founding_date = DateField('Asutamiskuupäev', validators=[DataRequired()], format='%Y-%m-%d')
+    capital = IntegerField('Kogukapital', validators=[DataRequired(), NumberRange(min=2500)])
+    shareholder_name = StringField('Osaniku nimi', validators=[DataRequired(), Length(min=3, max=100)])
+    shareholder_personalcode = IntegerField('Osaniku isikukood/registrikood', validators=[DataRequired(), NumberRange(min=1000000, max=99999999999)])
+    shareholder_share = IntegerField('Osaniku osa', validators=[DataRequired(), NumberRange(min=1)])
 
-class SearchForm(FlaskForm):
-    search_term = StringField('Search Term')
 
-
-class ShareholderForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=100)])
-    id = StringField('ID', validators=[DataRequired()])
-    share_size = IntegerField('Share Size (in EUR)', validators=[DataRequired(), NumberRange(min=1)])
-
-
-class CreateCompanyForm(FlaskForm):
-    name = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=100)])
-    registration_code = StringField('Registration Code', validators=[DataRequired()])
-    founding_date = DateField('Founding Date', validators=[DataRequired()])
-    total_capital = IntegerField('Total Capital (in EUR)', validators=[DataRequired(), NumberRange(min=2500)])
-    shareholders = FieldList(FormField(ShareholderForm))
+class IncreaseCapitalForm(FlaskForm):
+    shareholder_name = StringField('Osaniku nimi', validators=[DataRequired(), Length(min=3, max=100)])
+    shareholder_personalcode = IntegerField('Osaniku isikukood/registrikood', validators=[DataRequired(), NumberRange(min=1000000, max=99999999999)])
+    shareholder_share = IntegerField('Osaniku osa', validators=[DataRequired(), NumberRange(min=1)])
